2ae0d8bbabed6f3a468cdf3d1a740a3ddf6566fa
Scorpionguy <lapinfedor11@gmail.com>
2022-11-10 22:15:10 +0300

    впервые на гитхабе как тут флексить

---
+++ Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace bulka
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(199, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(377, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Нажмите на кнопку чтобы доказать что булка не лох";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.No;
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(266, 239);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(191, 64);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Не лох";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.LocationChanged += new System.EventHandler(this.comeback);
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.MouseEnter += new System.EventHandler(this.button1_MouseEnter);
+            this.button1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.button1_MouseEnter);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::bulka.Properties.Resources.bulka;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace bulka
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void comeback(object sender, EventArgs e)
+        {
+            int w = Convert.ToInt32(ClientSize.Width);
+            int h = Convert.ToInt32(ClientSize.Height);
+            int ww = w / 2;
+            int hh = h / 2;
+            if (button1.Location.X > w)
+            {
+                button1.Location = new Point(button1.Location.X - ww, button1.Location.Y);
+            }
+            if (button1.Location.X < 0)
+            {
+                button1.Location = new Point(button1.Location.X + ww, button1.Location.Y);
+            }
+            if (button1.Location.Y > h)
+            {
+                button1.Location = new Point(button1.Location.X, button1.Location.Y - hh);
+            }
+            if (button1.Location.Y < 0)
+            {
+                button1.Location = new Point(button1.Location.X, button1.Location.Y + hh);
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+        public void button1_MouseEnter(object sender, EventArgs e)
+        {  
+            Random rnd = new Random();
+            int x = 1;
+            int y = -1;
+            int z = x * y;
+            for (int i = -1; i < 1; i++)
+            {
+                int value = rnd.Next(200, 350);
+                int value2 = rnd.Next(200, 350);
+                if (z >= 0)
+                {
+                    button1.Location = new Point(button1.Location.X + value, button1.Location.Y - value2);
+                }
+                else
+                {
+                    button1.Location = new Point(button1.Location.X - value, button1.Location.Y + value2);
+                }
+                z++;
+                int w = Convert.ToInt32(ClientSize.Width);
+                int h = Convert.ToInt32(ClientSize.Height);
+                int ww = w / 2;
+                int hh = h / 2;
+                if (button1.Location.X > w)
+                {
+                    button1.Location = new Point(button1.Location.X - ww, button1.Location.Y);
+                }
+                if (button1.Location.Y > h || button1.Location.Y < 0)
+                {
+                    button1.Location = new Point(button1.Location.X, button1.Location.Y - hh);
+                }
+                SoundPlayer soundPlayer = new SoundPlayer(@"C:\Users\lapin\source\repos\bulka\bulka\Properties\sound.wav");
+                soundPlayer.Play();
+                soundPlayer.PlayLooping();
+            }
+
+        }
+        private void button1_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Random rnd = new Random();
+            int x = 1;
+            int y = -1;
+            int z = x * y;
+            for (int i = -1; i < 1; i++)
+            {
+                int value = rnd.Next(200, 350);
+                int value2 = rnd.Next(200, 350);
+                if (z >= 0)
+                {
+                    button1.Location = new Point(button1.Location.X + value, button1.Location.Y - value2);
+                }
+                else
+                {
+                    button1.Location = new Point(button1.Location.X - value, button1.Location.Y + value2);
+                }
+                z++;
+                int w = Convert.ToInt32(ClientSize.Width);
+                int h = Convert.ToInt32(ClientSize.Height);
+                int ww = w / 2;
+                int hh = h / 2;
+                if (button1.Location.X > w)
+                {
+                    button1.Location = new Point(button1.Location.X - ww, button1.Location.Y);
+                }
+                if (button1.Location.Y > h || button1.Location.Y < 0)
+                {
+                    button1.Location = new Point(button1.Location.X, button1.Location.Y - hh);
+                }
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
